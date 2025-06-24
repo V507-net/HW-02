a6b65855a7dc4d397640f26ab87b95d74b9b8036
V507-net <V507-net@github.com>
2025-06-24 13:16:26 +0300

    commit Checkpoint_3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,4 @@
-
-
 import compare.StudComparator;
-
 import compare.UniverCoparator;
 import enums.studCompareType;
 import enums.univerCompareType;
@@ -9,7 +6,6 @@ import subjects.Student;
 import subjects.University;
 import utils.SuperComparator;
 import utils.importExcel;
-
 import java.io.IOException;
 import java.util.List;
 
@@ -17,33 +13,25 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        
         List<Student> studs = importExcel.readStudents("src/main/resources/universityInfo.xlsx");
         List<University> univers = importExcel.readUniversities("src/main/resources/universityInfo.xlsx");
 
-        StudComparator studCompare = SuperComparator.getComparator(studCompareType.fullName);
-        UniverCoparator univerCompare = SuperComparator.getComparator(univerCompareType.fullName);
+        StudComparator studCompare;// = SuperComparator.getComparator(studCompareType.fullName);
+        UniverCoparator univerCompare;// = SuperComparator.getComparator(univerCompareType.fullName);
 
-        System.out.println("Студенты:");
+        System.out.println("Студенты по ФИО:");
+        studCompare = SuperComparator.getComparator(studCompareType.fullName);
         studs.stream().sorted(studCompare).forEach(System.out::println);
-        System.out.println("Университеты:");
+        System.out.println("Студенты по ФИО в обратном порядке:");
+        studs.stream().sorted(studCompare.reversed()).forEach(System.out::println);
+        System.out.println("Студенты по курсу:");
+        studCompare = SuperComparator.getComparator(studCompareType.CourseNumber);
+        studs.stream().sorted(studCompare).forEachOrdered(System.out::println);
+
+
+        System.out.println("Университеты по профилю:");
+        univerCompare = SuperComparator.getComparator(univerCompareType.mainProfile);
         univers.stream().sorted(univerCompare).forEach(System.out::println);
-
-
-/*
-        System.out.println("Список студентов:");
-        for (Student student : studs)
-        {
-            System.out.println(student);
-        }
-
-        System.out.println("Список университетов:");
-        for (University university : univers)
-        {
-            System.out.println(university);
-        }
-*/
-
 
     }
 }
